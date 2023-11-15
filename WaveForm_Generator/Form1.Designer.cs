7195d426f16d2398f8aecebe869c3e7a224cd58e
Jason07311 <52784322+Jason07311@users.noreply.github.com>
2023-11-15 11:52:32 +0800

    Added Upload File feature
    feat - Select File and Upload File
    UI - Added several components

--- WaveForm_Generator/Form1.Designer.cs
+++ WaveForm_Generator/Form1.Designer.cs
@@ -28,18 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            formsPlot1 = new ScottPlot.FormsPlot();
+            panel1 = new Panel();
+            button5 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            formsPlot1.Location = new Point(246, 17);
+            formsPlot1.Margin = new Padding(4, 3, 4, 3);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(727, 331);
+            formsPlot1.TabIndex = 0;
+            formsPlot1.Load += formsPlot1_Load;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(23, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(253, 494);
+            panel1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(18, 112);
+            button5.Name = "button5";
+            button5.Size = new Size(200, 74);
+            button5.TabIndex = 10;
+            button5.Text = "Select Data Input";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(95, 66);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 21);
+            label3.TabIndex = 7;
+            label3.Text = "directory path..";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(18, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 21);
+            label2.TabIndex = 6;
+            label2.Text = "File Path:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(95, 456);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Generate Wave";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Sine", "Normal Plot 1", "Normal Plot 2" });
+            comboBox1.Location = new Point(97, 217);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(18, 219);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Function:";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(18, 13);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 30);
+            button1.TabIndex = 2;
+            button1.Text = "Add New Tab";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 50);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(977, 567);
+            tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(panel1);
+            tabPage1.Controls.Add(formsPlot1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(969, 539);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(969, 539);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(990, 629);
+            Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private Panel panel1;
+        private Button button1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Label label3;
+        private Label label2;
+        private Button button5;
     }
 }
--- WaveForm_Generator/Form1.cs
+++ WaveForm_Generator/Form1.cs
@@ -1,10 +1,49 @@
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace WaveForm_Generator
 {
     public partial class Form1 : Form
     {
+
+        string selectedInputFile = "Select a file";
+
         public Form1()
         {
             InitializeComponent();
+            label3.Text = selectedInputFile;
         }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = "TabPage " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage(title);
+            tabControl1.TabPages.Add(myTabPage);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Data_Input dataInput = new Data_Input();
+            dataInput.ShowDialog();
+            string path = dataInput.SendData().Split('\\').Last();
+            selectedInputFile = path;
+            label3.Text = selectedInputFile;
+        }
+
+
     }
 }

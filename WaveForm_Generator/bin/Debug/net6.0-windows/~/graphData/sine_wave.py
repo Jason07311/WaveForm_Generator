7219e17aced84e7acd8da7aeaab24a5c8d27225b
jo3zeph <joethamjoetham@gmail.com>
2023-11-16 16:45:21 +0800

    Added sine_wave.py and sine_wave_data.csv
    - sine_wave.py (script that generates .csv file for data inputs)
    - modify parameters on num_points, amplitude, frequency, sampling_rate to modify Sine Graph
    
    - sine_wave_data.csv (data inputs for a sine graph)

---
+++ WaveForm_Generator/bin/Debug/net6.0-windows/~/graphData/sine_wave.py
@@ -0,0 +1,27 @@
+import csv
+import math
+
+# Function to generate sine wave data
+def generate_sine_wave(num_points, amplitude, frequency, sampling_rate):
+    time_values = [i / sampling_rate for i in range(num_points)]
+    voltage_values = [amplitude * math.sin(2 * math.pi * frequency * t) for t in time_values]
+    return time_values, voltage_values
+
+# Parameters for the sine wave
+num_points = 1000  # Adjust the number of points as needed
+amplitude = 5.0
+frequency = 1.0  # Adjust the frequency as needed
+sampling_rate = 100.0  # Adjust the sampling rate as needed
+
+# Generate sine wave data
+time_data, voltage_data = generate_sine_wave(num_points, amplitude, frequency, sampling_rate)
+
+# Create CSV file
+csv_filename = 'sine_wave_data.csv'
+with open(csv_filename, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Time', 'Voltage'])
+    for time, voltage in zip(time_data, voltage_data):
+        csv_writer.writerow([time, voltage])
+
+print(f"CSV file '{csv_filename}' created successfully.")

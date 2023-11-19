3ec2aea23615fabe63b55ebc8e1ca7eb5f9c2282
jo3zeph <joethamjoetham@gmail.com>
2023-11-19 15:30:29 +0800

    Updated script_sine_wave.py
    - Added a function to validate for existing file name "sine_wave.csv". If exist the increments file name
    - Altered the parameters

--- WaveForm_Generator/bin/Debug/net6.0-windows/~/graphData/script_sine_wave.py
+++ WaveForm_Generator/bin/Debug/net6.0-windows/~/graphData/script_sine_wave.py
@@ -1,5 +1,6 @@
 import csv
 import math
+import os
 
 # Function to generate sine wave data
 def generate_sine_wave(num_points, amplitude, frequency, sampling_rate):
@@ -8,16 +9,22 @@ def generate_sine_wave(num_points, amplitude, frequency, sampling_rate):
     return time_values, voltage_values
 
 # Parameters for the sine wave
-num_points = 1000  # Adjust the number of points as needed
-amplitude = 5.0
-frequency = 1.0  # Adjust the frequency as needed
-sampling_rate = 100.0  # Adjust the sampling rate as needed
+num_points = 5000  # Adjust the number of points as needed
+amplitude = 500.0
+frequency = 10.0  # Adjust the frequency as needed
+sampling_rate = 50.0  # Adjust the sampling rate as needed
 
 # Generate sine wave data
 time_data, voltage_data = generate_sine_wave(num_points, amplitude, frequency, sampling_rate)
 
-# Create CSV file
+# Create a unique CSV filename
 csv_filename = 'sine_wave_data.csv'
+file_counter = 1
+
+while os.path.exists(csv_filename):
+    file_counter += 1
+    csv_filename = f'sine_wave_data_{file_counter}.csv'
+
 with open(csv_filename, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['Time', 'Voltage'])
@@ -25,5 +32,3 @@ with open(csv_filename, 'w', newline='') as csv_file:
         csv_writer.writerow([time, voltage])
 
 print(f"CSV file '{csv_filename}' created successfully.")
-
-
